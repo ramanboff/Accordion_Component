62ed14bc96be6428ab99334895e43122073bf5d8
ramanboff <bayramofframan@gmail.com>
2023-10-15 13:53:50 +0400

    added 0 to number

--- src/AccordionItem.jsx
+++ src/AccordionItem.jsx
@@ -1,7 +1,7 @@
 const AccordionItem = ({num, title, text}) => {
   return (
     <div className="item">
-      <p className="number">{num}</p>
+      <p className="number">{num<9? `0${num+1}`: num+1}</p>
       <p className="text">{title}</p>
       <p className="icon">-</p>
       <div className="content-box">{text}</div>
