9ef763348bc3691069e99439b5f070802cc9d113
ramanboff <bayramofframan@gmail.com>
2023-10-15 14:16:08 +0400

    if open show "-", if close show "+" condition crea

--- src/AccordionItem.jsx
+++ src/AccordionItem.jsx
@@ -10,8 +10,8 @@ const [isOpen, setIsOpen] = useState(false)
     <div className="item" onClick={() => setIsOpen(!isOpen)}>
       <p className="number">{num<9? `0${num+1}`: num+1}</p>
       <p className="title">{title}</p>
-      <p className="icon" >-</p>
-      {isOpen && <div className="content-box">{text}</div>}
+      <p className="icon" >{isOpen ?"-":"+"}</p>
+      {isOpen && <div className={isOpen? "open":"content-box"}>{text}</div>}
     </div>
    );
 }
