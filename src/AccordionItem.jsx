d478cc58545eb507ac6d2040260ce730b9f41178
ramanboff <bayramofframan@gmail.com>
2023-10-15 14:09:28 +0400

    open close function

--- src/AccordionItem.jsx
+++ src/AccordionItem.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
+
 const AccordionItem = ({num, title, text}) => {
+
+const [isOpen, setIsOpen] = useState(false)
+
+
+
   return (
-    <div className="item">
+    <div className="item" onClick={() => setIsOpen(!isOpen)}>
       <p className="number">{num<9? `0${num+1}`: num+1}</p>
-      <p className="text">{title}</p>
-      <p className="icon">-</p>
-      <div className="content-box">{text}</div>
+      <p className="title">{title}</p>
+      <p className="icon" >-</p>
+      {isOpen && <div className="content-box">{text}</div>}
     </div>
    );
 }
